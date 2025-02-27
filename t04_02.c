678b14c6cb85d2daba6630c7db87737f86033836
NikahPanjaitan <suchiapanjaitan@gmail.com>
2025-02-27 14:25:39 +0700

    update t04_02.c

--- t04_02.c
+++ t04_02.c
@@ -3,14 +3,20 @@
 
 #include <stdio.h>
 
+#include <stdio.h>
+#include <string.h>
+
 int main(int _argv, char **_argc) {
     char str[21]; 
-    scanf("%20[^\n]", str); 
     
-    for (int i = 0; str[i] != '\0'; i++) {
-        printf("%03d", (unsigned char)str[i]); 
+    if (fgets(str, sizeof(str), stdin) != NULL) {
+        str[strcspn(str, "\n")] = '\0'; 
+        
+        for (int i = 0; str[i] != '\0'; i++) {
+            printf("%03d", (unsigned char)str[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
     
     return 0;
 }
